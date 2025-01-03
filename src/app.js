d93cf87389fb4c5497ad0492ff83c7675ddc80f6
Gabriel-DAloia <g-f-d-m@hotmail.com>
2025-01-03 00:19:40 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function excusa() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let selectedWho = who[Math.floor(Math.random() * who.length)];
+  let selectedAction = action[Math.floor(Math.random() * action.length)];
+  let selectedWhat = what[Math.floor(Math.random() * what.length)];
+  let selectedWhen = when[Math.floor(Math.random() * when.length)];
 
+  return `${selectedWho} ${selectedAction} ${selectedWhat} ${selectedWhen}`;
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excusaGenerada = excusa();
+  document.getElementById('excuse').innerHTML = excusaGenerada;
 };
